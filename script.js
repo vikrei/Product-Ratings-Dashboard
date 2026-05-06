d393a584f0782f2afe9baa5425b34587235c3da2
Viktoria Rein <reinviktorija@gmail.com>
2026-05-06 23:34:26 +0200

    added api fetch

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 console.log('JS connected');
+async function getProducts() {
+
+    const status = document.querySelector("#status");
+
+    try {
+
+        status.textContent = "Loading product data...";
+
+        const response = await fetch("https://fakestoreapi.com/products");
+
+        const products = await response.json();
+
+        console.log(products);
+
+        status.textContent = "Products loaded successfully!";
+
+    } catch(error) {
+
+        console.log(error);
+
+        status.textContent = "Something went wrong while loading data.";
+
+    }
+
+}
+
+getProducts();
